b6e518cba88f382b0e43a41d9cbd9d02e1dcbf16
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-19 22:22:37 +0000

    UI swap completed

--- client/src/components/Swap.jsx
+++ client/src/components/Swap.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Popover , Radio, Input, Modal , message} from 'antd'
+import { Popover , Radio, Input, Modal , message, Button} from 'antd'
 import { SettingOutlined, ArrowDownOutlined, DownOutlined } from '@ant-design/icons'
 import tokenList from '../tokenList.json'
 
@@ -9,12 +9,29 @@ export default function Swap() {
   const [tokenTwoAmount, setTokenTwoAmount] = useState(null)
   const [tokenOne, setTokenOne] = useState(tokenList[0])
   const [tokenTwo, setTokenTwo] = useState(tokenList[1])
+  const [isOpen, setIsOpen] = useState(false)
+  const [changeToken, setChangeTone] = useState(1)
+
 
   const switchHandler = () => {
     const one = tokenOne
     const two = tokenTwo
     setTokenOne(two)
     setTokenTwo(one)
+  }
+
+  const openModal = (asset) => {
+    setChangeTone(asset)
+    setIsOpen(true)
+  }
+
+  const modifyToken = (i) => {
+    if (changeToken === 1) {
+      setTokenOne(tokenList[i])
+    } else {
+      setTokenTwo(tokenList[i])
+    }
+    setIsOpen(false)
   }
 
   const setting = (
@@ -33,39 +50,69 @@ export default function Swap() {
     </>
   )
   return (
-    <div className='tradeBox'>
-      <div className="tradeBoxHeader">
-        <h4>Swap</h4>
-        <Popover
-          content= {setting}
-          title="Settings"
-          trigger="click"
-          placement='bottomRight'
-        >
-          <SettingOutlined className='cog' />
-        </Popover>
+    <>
+      <Modal
+        open={isOpen}
+        footer={null}
+        onCancel={() => setIsOpen(false)}
+        title= "Select a token"
+      >
+        <div className="modelContent">
+          {tokenList?.map((token, i) => {
+            return (
+              <div
+                className='tokenChoice'
+                key={i}
+                onClick={()=> modifyToken(i)}
+
+              >
+                <img src={token.img} alt={token.ticker} className='tokenLogo' />
+                <div className="tokenChoiceNames">
+                  <div className='tokenName'>{token.name}</div>
+                  <div className='tokenTicker'>{token.ticker}</div>
+                </div>
+                
+              </div>
+            )
+          })}
+        </div>
+      </Modal>
+    
+      <div className='tradeBox'>
+        <div className="tradeBoxHeader">
+          <h4>Swap</h4>
+          <Popover
+            content= {setting}
+            title="Settings"
+            trigger="click"
+            placement='bottomRight'
+          >
+            <SettingOutlined className='cog' />
+          </Popover>
+        </div>
+        <div className="inputs">
+          <Input placeholder='0' value={tokenOneAmount} onChange={(e) => setTokenOneAmount(e.target.value)} />
+          <Input placeholder='0' value={tokenTwoAmount} disabled />
+
+          <div className='switchButton' onClick={switchHandler}>
+            <ArrowDownOutlined className='switchArrow'/>
+          </div>
+
+          <div className="assetOne" onClick={()=> openModal(1)}>
+            <img src={tokenOne.img} alt="assetOneLogo" className='assetLogo' />
+            {tokenOne.ticker}
+            <DownOutlined />
+          </div>
+
+          <div className="assetTwo" onClick={()=> openModal(2)}>
+            
+            <img src={tokenTwo.img} alt="assetTwoLogo" className='assetLogo' />
+            {tokenTwo.ticker}
+            <DownOutlined />
+          </div>
+        </div>
+        <Button className='swapButton' disabled={!tokenOneAmount}>Swap</Button>
       </div>
-      <div className="inputs">
-        <Input placeholder='0' value={tokenOneAmount} onChange={(e) => setTokenOneAmount(e.target.value)} />
-        <Input placeholder='0' value={tokenTwoAmount} disabled />
-
-        <div className='switchButton' onClick={switchHandler}>
-          <ArrowDownOutlined className='switchArrow'/>
-        </div>
-
-        <div className="assetOne">
-          <img src={tokenOne.img} alt="assetOneLogo" className='assetLogo' />
-          {tokenOne.ticker}
-          <DownOutlined />
-        </div>
-
-        <div className="assetTwo">
-          
-          <img src={tokenTwo.img} alt="assetTwoLogo" className='assetLogo' />
-          {tokenTwo.ticker}
-          <DownOutlined />
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
