37ebb8175547b29f87e061f8943469534de9b534
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-25 18:08:29 +0000

    removed the issue from api

--- api/index.js
+++ api/index.js
@@ -30,6 +30,6 @@ app.use(express.json())
 
 
 app.use(express.static(path.join(__dirname, '../client/dist')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'../client/dist/index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname,'../client/dist/index.html'));
+// })
