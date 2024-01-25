c0add2905cb42103b73f50165356e462f71927ef
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-25 19:09:02 +0000

    proxy switched

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,16 +5,17 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-       '/api': {
-        target: "http://localhost:3000",
-        secure: false,
-    },
       '/swap': {
         target: "https://api.1inch.dev",
         secure: false,
         changeOrigin: true
-      }
+      }, 
+       '/api': {
+        target: "http://localhost:3000",
+        secure: false,
+    }
     }
   },
+
   plugins: [react()],
 })
