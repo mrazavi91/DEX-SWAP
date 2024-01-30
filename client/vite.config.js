58fad32edf9180556f9ae242334b2cfc1206bed1
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-30 17:21:57 +0000

    config vite changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,16 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/swap': {
-        target: "https://api.1inch.dev",
-        changeOrigin: true,
-        secure: false
-      }, 
-       '/api': {
-        target: "http://localhost:3000",
-         secure: false,
-        changeOrigin: true
-    }
+      '/api': "http://localhost:3000",
+      secure: false
     }
   },
 
